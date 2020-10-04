61a40abf3a0fb6c882ff0bd72bb12386f8931fb1
Lina <gusbomli@student.gu.se>
2020-10-04 20:21:33 +0200

    Lina

--- Controller/Scan.java
+++ Controller/Scan.java
@@ -2,16 +2,42 @@ package Controller;
 import java.util.Scanner;
 
 
+
+
+
+
+
 public class Scan {
 
     static Scanner scan = new Scanner(System.in);
 
     // Methods for reading input without printing
-    public static String Scan(){ return scan.next(); }
-    public static String ScanLine(){ return scan.nextLine(); }
-    public static int ScanInt(){ return scan.nextInt();}
-    public static double ScanDouble(){ return scan.nextDouble();}
-    public static long ScanLong(){  return scan.nextLong();}
+    public static String Scan(){
+        return scan.next();
+    }
+
+    public static String ScanLine(){
+        return scan.nextLine();
+    }
+
+    public static int ScanInt(){
+    //    scan.nextInt();
+    //    scan.nextLine();
+        return scan.nextInt();
+    }
+    public static double ScanDouble(){
+    //    scan.nextDouble();
+    //    scan.nextLine();
+
+        return scan.nextDouble();
+    }
+
+    public static long ScanLong(){
+    //    scan.nextLong();
+    //    scan.nextLine();
+        return scan.nextLong();
+
+    }
 
     // Method for reading user input and printing at the same time
     public static String readLine (String output){
@@ -28,4 +54,10 @@ public class Scan {
         System.out.println(output);
         return scan.nextDouble();
     }
+  // public static double readDouble (String output){
+  //     System.out.println(output);
+    // double input = scan.nextDouble;
+    // scan.nextLine();
+  //     return input;
+  // }
 }
