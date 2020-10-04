29850b8e8cd8e3216971ea685e649e84f3dbf137
Emilie Anthony <emilieanthony@live.se>
2020-10-04 21:58:59 +0200

    Started to create 8.2. Method for asking customer about membership done, next step is to implement a new array list to store all requests and let an employee change the membership + change menu.

--- View/ViewCustomer.java
+++ View/ViewCustomer.java
@@ -19,7 +19,7 @@ public class ViewCustomer {
                 System.out.println("3. Return to Main Menu");
                 // on first page after login options should be view return rent review etc
 
-                int userOption = Scan.ScanInt();
+                int userOption = Scan.readInt();
 
                 if (userOption == 1) { //change to switch bc this will be more than 3 menu options
                     controllerCustomer.rentItem(/*customer*/);
@@ -31,5 +31,7 @@ public class ViewCustomer {
                     System.out.println("Invalid input.");
                 }
             }
+
+
 
         }
